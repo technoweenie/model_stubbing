8e9bcef1efa600fedc2539a7e864e72a18f46049
rick <technoweenie@gmail.com>
2008-01-28 16:00:35 -0800

    update for post rails 2.0.  ActiveRecord::Fixtures uses #setup_fixtures and #teardown_fixtures.  no more method_added whammy jammy

--- lib/model_stubbing/extensions.rb
+++ lib/model_stubbing/extensions.rb
@@ -12,30 +12,9 @@ module ModelStubbing
       def create_model_methods_for(models)
         class_eval models.collect { |model| model.stub_method_definition }.join("\n")
       end
-
-      def self.method_added(method)
-        case method.to_s
-        when 'setup'
-          unless method_defined?(:setup_without_model_stubs)
-            alias_method :setup_without_model_stubs, :setup
-            define_method(:setup) do
-              setup_with_model_stubs
-              setup_without_model_stubs
-            end
-          end
-        when 'teardown'
-          unless method_defined?(:teardown_without_model_stubs)
-            alias_method :teardown_without_model_stubs, :teardown
-            define_method(:teardown) do
-              teardown_without_model_stubs
-              teardown_with_model_stubs
-            end
-          end
-        end
-      end
     end
 
-    def setup_with_model_stubs
+    def setup_fixtures
       return unless self.class.definition
       unless self.class.definition_inserted
         self.class.definition.insert!
@@ -43,12 +22,10 @@ module ModelStubbing
       end
       self.class.definition.setup_test_run
     end
-    alias_method :setup, :setup_with_model_stubs
 
-    def teardown_with_model_stubs
+    def teardown_fixtures
       self.class.definition && self.class.definition.teardown_test_run
     end
-    alias_method :teardown, :teardown_with_model_stubs
 
     def stubs(key)
       self.class.definition && self.class.definition.stubs[key]
